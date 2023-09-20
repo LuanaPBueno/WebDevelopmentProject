f70d7d01d9435b9f652ec088d0416a3ae12868c8
guilhermesenko <117857836+guilhermesenko@users.noreply.github.com>
2023-09-20 13:26:45 -0300

    Update Sql01Modelo.sql

--- Sql01Modelo.sql
+++ Sql01Modelo.sql
@@ -2,6 +2,7 @@ CREATE TABLE Disciplinas (
     cod INT PRIMARY KEY,
     nome_da_disciplina VARCHAR(255),
     créditos INT,
-    pré_requisito VARCHAR(255),
+    pre_requisito VARCHAR(255),
+    co_requesito VARCHAR(255),
     ementa VARCHAR(255)
 );
