c358961ed5a9f44a6dc9539c24d4f178b99fda84
guilhermesenko <117857836+guilhermesenko@users.noreply.github.com>
2023-09-26 10:22:47 -0300

    Updated main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-Testando
+from __future__ import annotations
+from services.web_scrapper import WebScrapper
+
+subject = WebScrapper.get_subject_from_code("CTC4002")
+print(subject.code, subject.name, subject.credits_amount)
+
+for prerequisite in subject.prerequisites:
+    print(prerequisite)
