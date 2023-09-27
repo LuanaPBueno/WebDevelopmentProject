255777df75298e725815a40eb580adfb55a794d8
LuanaPBueno <140860101+LuanaPBueno@users.noreply.github.com>
2023-09-27 12:27:54 +0000

    new

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-<<<<<<< HEAD
-from services.web_scrapper import WebScrapper
+# <<<<<<< HEAD
+# from services.web_scrapper import WebScrapper
 
-print(WebScrapper.get_courses())
-=======
-from __future__ import annotations
-from services.web_scrapper import WebScrapper
+# print(WebScrapper.get_courses())
+# =======
+# from __future__ import annotations
+# from services.web_scrapper import WebScrapper
 
-subject = WebScrapper.get_subject_from_code("CTC4002")
-print(subject.code, subject.name, subject.credits_amount)
+# subject = WebScrapper.get_subject_from_code("CTC4002")
+# print(subject.code, subject.name, subject.credits_amount)
 
-for prerequisite in subject.prerequisites:
-    print(prerequisite)
->>>>>>> c358961ed5a9f44a6dc9539c24d4f178b99fda84
+# for prerequisite in subject.prerequisites:
+#     print(prerequisite)
+# >>>>>>> c358961ed5a9f44a6dc9539c24d4f178b99fda84
