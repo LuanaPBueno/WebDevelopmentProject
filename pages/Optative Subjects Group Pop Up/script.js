b6f4b42f40afb01c61016f7189610bec2ea86db9
Benito <benitoapepe@yahoo.com.br>
2023-11-14 11:36:20 -0300

    Update Optative Subjects Group Pop Up script.js
    Co-Authored-By: R0bbyt3 <130287531+R0bbyt3@users.noreply.github.com>

--- pages/Optative Subjects Group Pop Up/script.js
+++ pages/Optative Subjects Group Pop Up/script.js
@@ -1,4 +1,4 @@
-import { getOptativeSubjectsGroup } from "../../services/firebase/firebase.js";
+import { getOptativeSubjectsGroup, getSubject } from "../../services/firebase/firebase.js";
 
 document.addEventListener("DOMContentLoaded", async function () {
   const form = document.getElementById('searchForm');
@@ -15,12 +15,29 @@ document.addEventListener("DOMContentLoaded", async function () {
       if (subjectsGroup && subjectsGroup.subjects) {
         for (const key in subjectsGroup.subjects) {
           const subjectName = subjectsGroup.subjects[key];
-          const subjectA = document.createElement("a");
-          subjectA.textContent = subjectName + " (" + key + ")";
-          subjectA.href = "https://www.puc-rio.br/ferramentas/ementas/ementa.aspx?cd=" + key;
-          subjectA.className = "subjectoption";
-          subjectsDiv.appendChild(subjectA);
-          subjectsDiv.appendChild(document.createElement("br"));
+
+          try {
+            const getSub = await getSubject(subjectName);
+
+            if (getSub && getSub.name && getSub.code) {
+
+              const subjectsNameString = getSub.name;
+
+              const subjectA = document.createElement("a");
+              subjectA.textContent = subjectsNameString;
+              subjectA.href = "https://www.puc-rio.br/ferramentas/ementas/ementa.aspx?cd=" + getSub.code;
+              subjectA.className = "subjectoption";
+              subjectsDiv.appendChild(subjectA);
+              subjectsDiv.appendChild(document.createElement("br"));
+
+            } else {
+              subjectsDiv.textContent = 'Nenhuma matéria encontrada para o código: ' + subjectName;
+            }
+
+          } catch (error) {
+            console.error('Erro ao buscar dados:', error);
+            subjectsDiv.textContent = 'Erro ao buscar matéria. Tente novamente mais tarde.';
+          }  
         }
       } else {
         subjectsDiv.textContent = 'Nenhuma matéria encontrada para o código: ' + inputCode;
